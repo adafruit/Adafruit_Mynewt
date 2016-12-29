eb57fbeb2282c3f7c342def997af8bb239d11ab5
microbuilder <contact@microbuilder.eu>
2016-12-29 18:08:35 +0100

    Added accel only orientation tester

--- apps/mfgtest/src/main.c
+++ apps/mfgtest/src/main.c
@@ -32,8 +32,9 @@
 
 #include "tsl2561/tsl2561.h"
 
-#include <sensor/sensor.h>
+#include "sensor/sensor.h"
 #include "lsm303dlhc/lsm303dlhc.h"
+#include "orientation/orientation.h"
 
 struct lsm303dlhc lsm303dlhc_sensor;
 
@@ -54,13 +55,20 @@ static struct os_task task_sysevq;
 /* Event queue for events handled by the system (shell, etc.) */
 static struct os_eventq sys_evq;
 
-// Command handler prototype declaration
+/* Command handler prototype declaration */
 static int shell_i2cscan_cmd(int argc, char **argv);
+static int shell_orientation_cmd(int argc, char **argv);
 
-// Shell command struct
+/* i2cscan shell command struct */
 static struct shell_cmd shell_i2cscan_cmd_struct = {
     .sc_cmd = "i2cscan",
     .sc_cmd_func = shell_i2cscan_cmd
+};
+
+/* orientation command shell struct */
+static struct shell_cmd shell_orientation_cmd_struct = {
+    .sc_cmd = "orientation",
+    .sc_cmd_func = shell_orientation_cmd
 };
 
 /**
@@ -87,7 +95,7 @@ shell_i2cscan_cmd(int argc, char **argv)
                    "     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n"
                    "00:                         ");
 
-    /* Scan all valid I2C addresses (0x03..0x77) */
+    /* Scan all valid I2C addresses (0x08..0x77) */
     for (addr = 0x08; addr < 0x78; addr++) {
         int rc = hal_i2c_master_probe(0, addr, timeout);
         /* Print addr header every 16 bytes */
@@ -104,6 +112,60 @@ shell_i2cscan_cmd(int argc, char **argv)
     }
     console_printf("\nFound %u devices on I2C bus 0\n", dev_count);
 
+    return 0;
+}
+
+static int
+orientation_listener(struct sensor *sensor, void *arg, void *data)
+{
+    int rc;
+    struct sensor_accel_data *sad;
+    struct or_orientation_vec orv;
+
+    memset(&orv, 0, sizeof(orv));
+
+    sad = (struct sensor_accel_data *) data;
+
+    rc = or_from_accel(sad, &orv);
+
+    console_printf("r = %i, ", (int)orv.roll);
+    console_printf("p = %i ", (int)orv.pitch);
+    console_printf("\n");
+
+    return rc;
+}
+
+/* orientation command handler */
+static int
+shell_orientation_cmd(int argc, char **argv)
+{
+    struct sensor *sensor;
+    struct sensor_listener listener;
+    int rc;
+
+    /* Look up sensor by name */
+    sensor = sensor_mgr_find_next_bydevname("lsm303dlhc", NULL);
+    if (!sensor) {
+        console_printf("Sensor %s not found!\n", "lsm303dlhc");
+    }
+
+    listener.sl_sensor_type = SENSOR_TYPE_ACCELEROMETER;
+    listener.sl_func = orientation_listener;
+    listener.sl_arg = NULL;
+    //listener.sl_arg = &ctx;
+
+    rc = sensor_register_listener(sensor, &listener);
+    if (rc != 0) {
+        goto err;
+    }
+
+    /* Get a fresh data sample (will be returned in the listener) */
+    rc = sensor_read(sensor, SENSOR_TYPE_ACCELEROMETER,
+        NULL, NULL, OS_TIMEOUT_NEVER);
+
+    sensor_unregister_listener(sensor, &listener);
+
+err:
     return 0;
 }
 
@@ -168,7 +230,8 @@ lsm303dlhc_drvr_init(struct os_dev *dev, void *arg)
 
     cfg.accel_range = LSM303DLHC_ACCEL_RANGE_2;
     cfg.accel_rate = LSM303DLHC_ACCEL_RATE_400;
-    cfg.sample_itvl = OS_TICKS_PER_SEC / 200;
+    cfg.mag_gain = LSM303DLHC_MAG_GAIN_1_3;
+    cfg.mag_rate = LSM303DLHC_MAG_RATE_220;
 
     rc = lsm303dlhc_config((struct lsm303dlhc *) dev, &cfg);
     if (rc != 0) {
@@ -204,6 +267,7 @@ main(int argc, char **argv)
 
 #if MYNEWT_VAL(SHELL_TASK)
     shell_cmd_register(&shell_i2cscan_cmd_struct);
+    shell_cmd_register(&shell_orientation_cmd_struct);
 #endif
 
     rc = init_tasks();
