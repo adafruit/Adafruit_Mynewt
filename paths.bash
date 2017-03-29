b78ef0bcb311b393854b86fa3bc573451444e953
microbuilder <contact@microbuilder.eu>
2017-03-29 16:47:37 +0200

    Went back to 4.9 for consistency

--- paths.bash
+++ paths.bash
@@ -5,7 +5,7 @@ export GOPATH=$HOME/prog/go
 export PATH="$GOPATH"/bin/:$PATH
 
 # GCC paths
-#export PATH=$PATH:$HOME/prog/gcc-arm-none-eabi-4_9-2015q1/bin
-export PATH=$PATH:$HOME/prog/gcc-arm-none-eabi-5_4-2016q3/bin
+export PATH=$PATH:$HOME/prog/gcc-arm-none-eabi-4_9-2015q1/bin
+#export PATH=$PATH:$HOME/prog/gcc-arm-none-eabi-5_4-2016q3/bin
 #export PATH=$PATH:/usr/local/Cellar/gcc/5.3.0/bin
 export PATH=$PATH:/usr/local/Cellar/gcc/6.2.0/bin
